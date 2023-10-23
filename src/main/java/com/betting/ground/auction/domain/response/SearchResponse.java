3f494d2b7d40df469da441aa861a7889d5df7de8
Harok <41049683+dorianharok@users.noreply.github.com>
2023-10-23 18:24:11 +0900

    Paging 변수 수정 #5

--- src/main/java/com/betting/ground/auction/domain/response/SearchResponse.java
+++ src/main/java/com/betting/ground/auction/domain/response/SearchResponse.java
@@ -15,12 +15,8 @@ import java.util.List;
 public class SearchResponse {
 
     private List<AuctionInfo> content;
-    @Schema(description = "전체 데이터 개수", example = "99")
-    private Integer totalCount;
     @Schema(description = "전체 페이지 수", example = "10")
     private Integer totalPages;
     @Schema(description = "현재 페이지", example = "1")
     private Integer currentPage;
-    @Schema(description = "마지막 페이지 여부", example = "false")
-    private Boolean isEndPage;
 }
