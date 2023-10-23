88003b95b59253f9ee22c8a0087234da6a4e9554
PalaceKant <86561865+PalaceKant@users.noreply.github.com>
2023-10-23 18:37:32 +0900

    [Feat] Swagger API 문서화 #5
     프로필 정보 (api/user)
        프로필 조회 - ==
        닉네임 수정 -  (/nickname)
        계좌번호 등록 - (/account)
        계좌번호 수정 - (/account)
        주소 등록 - (/address)

--- src/main/java/com/betting/ground/user/dto/UserAccountDTO.java
+++ src/main/java/com/betting/ground/user/dto/UserAccountDTO.java
@@ -1,5 +1,7 @@
 package com.betting.ground.user.dto;
 import java.time.LocalDate;
+
+import com.betting.ground.user.domain.Bank;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +13,9 @@ import lombok.NoArgsConstructor;
 
 public class UserAccountDTO {
     //계좌번호 등록,수정
-    @Schema(description = "계좌번호", example = "4321-0987-6543")
-    private String accountNumber;
+    @Schema(description = "은행명", example = "국민은행")
+    private String bankName;
+
+    @Schema(description = "계좌번호", example = "1234-5678-9101")
+    private String bankAccount;
 }
--- src/main/java/com/betting/ground/user/dto/UserAddressDTO.java
+++ src/main/java/com/betting/ground/user/dto/UserAddressDTO.java
@@ -10,7 +10,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class UserAddressDTO {
+
     //주소 등록,수정
-    @Schema(description = "주소", example = "서울특별시 용산구")
-    private String address;
+    @Schema(description = "도로명주소", example = "서울특별시 양천구 목동중앙북로 1길")
+    private String roadName;
+
+    @Schema(description = "지번주소", example = "서울특별시 양천구 123-4")
+    private String addressName;
+
+    @Schema(description = "우편번호", example = "19524")
+    private String zipcode;
+
+    @Schema(description = "상세주소", example = "1011호")
+    private String detailAddress;
+
 }
