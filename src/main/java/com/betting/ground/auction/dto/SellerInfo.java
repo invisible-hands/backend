c546d5907fe5942e3a71251f03584fa5a49bad1e
PanjunKwon <pndlm1979@naver.com>
2023-10-26 15:24:40 +0900

    [feat] swagger api 수정 #5
    상품 등록 api 응답 데이터에 판매자 프로필 사진 추가

--- src/main/java/com/betting/ground/auction/dto/SellerInfo.java
+++ src/main/java/com/betting/ground/auction/dto/SellerInfo.java
@@ -12,6 +12,8 @@ public class SellerInfo {
     private Long sellerId;
     @Schema(description = "판매자 닉네임", example="biggu")
     private String nickname;
+    @Schema(description = "판매자 프로필 이미지", example="profileImage-s3-url")
+    private String profileImage;
     @Schema(description = "판매자 경매글 수", example="5")
     private int auctionCnt;
     @Schema(description = "판매자 경매글 목록 3개만 보여줌")
