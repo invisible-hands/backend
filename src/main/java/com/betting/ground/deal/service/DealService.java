4c27250ed6d5223b4015171042522e07bee615f2
Harok <41049683+dorianharok@users.noreply.github.com>
2023-10-26 21:53:07 +0900

    [Feat] 구매 확정 api 구현 #28

--- src/main/java/com/betting/ground/deal/service/DealService.java
+++ src/main/java/com/betting/ground/deal/service/DealService.java
@@ -1,15 +1,38 @@
 package com.betting.ground.deal.service;
 
+import com.betting.ground.auction.repository.AuctionRepository;
+import com.betting.ground.common.exception.ErrorCode;
+import com.betting.ground.common.exception.GlobalException;
+import com.betting.ground.deal.domain.Deal;
+import com.betting.ground.deal.domain.DealStatus;
+import com.betting.ground.deal.repository.DealRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class DealService {
 
-    public void purchaseComplete(Long dealId) {
+    private final AuctionRepository auctionRepository;
+    private final DealRepository dealRepository;
 
+//    public PurchaseInfoResponse getAllPurchases(Long userId, Pageable pageable){
+//        return new PurchaseInfoResponse(dealRepository.getAllPurchases(userId, pageable));
+//    }
+//
+//    public PurchaseInfoResponse getBeforeShippingPurchases(Long userId, Pageable pageable) {
+//        return new PurchaseInfoResponse(dealRepository.getBeforePurchases(userId, pageable));
+//    }
+
+    public void purchaseComplete(Long dealId) {
+        Deal deal = dealRepository.findById(dealId).orElseThrow(
+                () -> new GlobalException(ErrorCode.DEAL_NOT_FOUND)
+        );
+
+        deal.updateStatus(DealStatus.PURCHASE_COMPLETE);
     }
 }
