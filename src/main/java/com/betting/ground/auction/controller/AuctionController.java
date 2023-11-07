789afb45d3db3f87b1959756b3337c647f973122
jsjune <avraskio@gmail.com>
2023-11-07 12:47:41 +0900

    [Fix] 쿠키 npe 수정 #72

--- src/main/java/com/betting/ground/auction/controller/AuctionController.java
+++ src/main/java/com/betting/ground/auction/controller/AuctionController.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.UUID;
 
 
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auction")
@@ -37,7 +36,7 @@ import java.util.UUID;
 public class AuctionController {
 
     private final AuctionService auctionService;
-  
+
     @GetMapping("/{auctionId}")
     @Operation(summary = "경매 상세 정보", description = "")
     public Response<ItemDetailDto> getItemDetail(
@@ -47,14 +46,18 @@ public class AuctionController {
             HttpServletRequest request,
             HttpServletResponse response
     ) {
-        Cookie cookie = Arrays.stream(request.getCookies())
-                .filter(c -> c.getName().equals("UserUUID"))
-                .findFirst().orElse(
-                        new Cookie("UserUUID", UUID.randomUUID().toString())
-                );
+        Cookie cookie;
+        if (request.getCookies() == null) {
+            cookie = new Cookie("UserUUID", UUID.randomUUID().toString());
+        } else {
+            cookie = Arrays.stream(request.getCookies())
+                    .filter(c -> c.getName().equals("UserUUID"))
+                    .findFirst().orElse(
+                            new Cookie("UserUUID", UUID.randomUUID().toString())
+                    );
+        }
         cookie.setMaxAge(24 * 60 * 60);
         response.addCookie(cookie);
-
         return Response.success("해당 경매글 보기 성공", auctionService.getItemDetail(loginUser, auctionId, cookie.getValue()));
     }
 
@@ -117,7 +120,7 @@ public class AuctionController {
     public Response<Void> bid(@PathVariable Long auctionId,
                               @RequestBody PayRequest request,
                               @AuthenticationPrincipal LoginUser loginUser) {
-        auctionService.bid(auctionId,request, loginUser.getUser().getId());
+        auctionService.bid(auctionId, request, loginUser.getUser().getId());
         return Response.success("경매 참여 완료", null);
     }
 
@@ -161,7 +164,7 @@ public class AuctionController {
 
     @GetMapping("/{auctionId}/bid")
     @Operation(summary = "상품 입찰하기 페이지 조회")
-    public Response<BidInfoResponse> getBidInfo(@PathVariable Long auctionId, @AuthenticationPrincipal LoginUser loginUser){
+    public Response<BidInfoResponse> getBidInfo(@PathVariable Long auctionId, @AuthenticationPrincipal LoginUser loginUser) {
         return Response.success("입찰 조회 완료", auctionService.getBidInfo(auctionId, loginUser.getUser().getId()));
     }
 }
