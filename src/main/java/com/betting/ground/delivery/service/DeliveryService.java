749c90456934e2769d3b0af21b796334f20c8767
Harok <41049683+dorianharok@users.noreply.github.com>
2023-11-07 17:46:54 +0900

    fix. 경매의 주인만 송장번호 등록 가능 #75

--- src/main/java/com/betting/ground/delivery/service/DeliveryService.java
+++ src/main/java/com/betting/ground/delivery/service/DeliveryService.java
@@ -24,7 +24,7 @@ public class DeliveryService {
     private final AuctionRepository auctionRepository;
     private final DealRepository dealRepository;
 
-    public DeliveryInfoResponse create(DeliveryInfoRequest request){
+    public DeliveryInfoResponse create(Long userId, DeliveryInfoRequest request){
         Auction auction = auctionRepository.findById(request.getAuctionId()).orElseThrow(
                 () -> new GlobalException(ErrorCode.AUCTION_NOT_FOUND, "존재하지 않는 경매글입니다.")
         );
@@ -32,9 +32,14 @@ public class DeliveryService {
                 () -> new GlobalException(ErrorCode.DEAL_NOT_FOUND)
         );
 
-        if(deal.getDealStatus() != DealStatus.DELIVERY_WAITING){
-            throw new GlobalException(ErrorCode.BAD_REQUEST);
+        if(!deal.getSellerId().equals(userId)){
+            throw new GlobalException(ErrorCode.BAD_REQUEST, "본인이 올린 게시글이 아닌데 요청");
         }
+
+        if(deal.getDealStatus() != DealStatus.DELIVERY_WAITING){
+            throw new GlobalException(ErrorCode.BAD_REQUEST, "배송중 상태가 아닌 상태에서 송장번호 등록 시도");
+        }
+
 
         deal.updateStatus(DealStatus.PURCHASE_COMPLETE_WAITING);
 
